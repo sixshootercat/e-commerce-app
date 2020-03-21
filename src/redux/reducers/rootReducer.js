27d8976401911792f090279d7f0914acc48348e8
unknown <kevin.ruhl92@gmail.com>
2020-03-21 20:11:31 -0300

    adding redux persist to persist data in local storage, creating directory reducer to store homepage state, adding directory selector to access directory state in directory component

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,10 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./userReducer";
 import cartReducer from "./cartReducer";
+import directoryReducer from "./directoryReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
-import rootReducer from './reducers/rootReducer'
+import rootReducer from "./reducers/rootReducer";
 
-const middlewares = [logger]
+const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store
+export const persistor = persistStore(store);
