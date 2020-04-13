0cf6b373c52e1d89cb98c7f47eb53dd96038847f
Kevin Ruhl <kevin.ruhl92@gmail.com>
2020-04-13 02:24:03 -0300

    adding type property to custom button component in sign in page

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = async e => {
+  handleSubmit = async (e) => {
     e.preventDefault();
 
     const { email, password } = this.state;
@@ -29,7 +29,7 @@ class SignIn extends React.Component {
     }
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     const { value, name } = e.target;
 
     this.setState({ [name]: value });
@@ -60,7 +60,11 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type='button'
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in With Google
             </CustomButton>
           </div>
--- src/redux/selectors/shopSelectors.js
+++ src/redux/selectors/shopSelectors.js
@@ -1,31 +1,31 @@
 import { createSelector } from 'reselect';
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  shop => shop.collections
+  (shop) => shop.collections
 );
 
 // Selector to convert an object into an array
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections =>
-    collections ? Object.keys(collections).map(key => collections[key]) : []
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
-// Collections state object
-export const selectCollection = collectionUrlParam =>
-  createSelector([selectCollections], collections =>
+// Collection state object
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
     collections ? collections[collectionUrlParam] : null
   );
 
 export const selectIsCollectionFetching = createSelector(
   [selectShop],
-  shop => shop.isFetching
+  (shop) => shop.isFetching
 );
 
 export const selectIsCollectionsLoaded = createSelector(
   [selectShop],
-  shop => !!shop.collections
+  (shop) => !!shop.collections
 );
