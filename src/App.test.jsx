64829be44e8aeed437d75514469f8de7a98d1548
Kevin Ruhl <kevin@blockfi.com>
2020-06-08 03:38:59 -0300

    testing ci workflow

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('App Component', () => {
+  it('renders correctly without crashing', () => {
+    return undefined;
+  });
+});
