a2a626747bcd5dc0ce30c20b66ea313f5fb36e9a
Kevin Ruhl <kevin@blockfi.com>
2020-06-08 03:44:38 -0300

    tweaking yml file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
 describe('App Component', () => {
   it('renders correctly without crashing', () => {
     return undefined;
