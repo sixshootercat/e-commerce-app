62da19ebaec23b0ba9e2b12bf147159037c136ac
Kevin Ruhl <kevin.ruhl92@gmail.com>
2020-06-08 09:18:59 -0300

    small change

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,6 @@ describe('App Component', () => {
   it('renders correctly without crashing', () => {
     let num1,
       num2 = 2;
-    let results = num1 + num2;
+    let result = num1 + num2;
   });
 });
