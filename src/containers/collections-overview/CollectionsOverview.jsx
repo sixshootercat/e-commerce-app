5999a67382e0ab42e0484293caff9b9051cd12df
Kevin Ruhl <kevin.ruhl92@gmail.com>
2020-07-30 01:08:49 -0300

    Changes handling of loading screen in CollectionPage and CollectionOverview from hoc pattern to now conditionally rendering Spinner inside each respective component, setting collection to empty object instead of null in selector

--- src/containers/collections-overview/CollectionsOverview.jsx
+++ src/containers/collections-overview/CollectionsOverview.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './collections-overview.scss';
 import { useSelector } from 'react-redux';
-
-import { selectCollectionsForPreview } from 'redux/selectors/shopSelectors';
+import {
+  selectCollectionsForPreview,
+  selectIsCollectionsLoaded,
+} from 'redux/selectors/shopSelectors';
+import Spinner from 'components/spinner/Spinner';
 
 import CollectionPreview from 'containers/collection-preview/CollectionPreview';
 
 const CollectionsOverview = () => {
   const collections = useSelector(selectCollectionsForPreview);
+  const isLoaded = useSelector(selectIsCollectionsLoaded);
 
   return (
-    <div className='collections-overview'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
+    <>
+      {!isLoaded ? (
+        <Spinner />
+      ) : (
+        <div className='collections-overview'>
+          {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
